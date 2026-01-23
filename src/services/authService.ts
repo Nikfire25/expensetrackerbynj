f72773322fffc45c069b1fed8b99b8001501b703
Nikunj Jain <nikunjjain@Nikunjs-MacBook-Air.local>
2026-01-23 08:50:47 +0530

    removed environment variablesx

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://expensetrackerbynjbackend.onrender.com";
 
 export interface LoginPayload {
   email: string;
